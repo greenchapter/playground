8a2a1e012333482e49b335b4428247729430ba37
Thomas Ott <thomas.ott@greenchapter.de>
2019-12-17 10:04:29 +0100

    feat(arrow-functions): add a first arrow function (#4)

---
+++ javascript/arrow-functions.js
@@ -0,0 +1,18 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+let foo = () => {
+	console.log('💘 arrow functions')
+}
+
+foo()
+
+
+var materials = [
+  'Hydrogen',
+  'Helium',
+  'Lithium',
+  'Beryllium'
+];
+
+//(param1, param2, …, paramN) => { statements }
+// singleParam => statements
+console.log(materials => materials);
