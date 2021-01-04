1f1fd21e244d6fcc5dc0a3e5309c7cb4576b34fd
Thomas Ott <thomas.ott@greenchapter.de>
2021-01-04 12:39:40 +0100

    feat: iris plot

--- data-science/README.md
+++ data-science/README.md
@@ -10,12 +10,22 @@ https://docs.python.org/3/tutorial/venv.html
 python3 -m venv data-science
 
 
+which python3
+
+
+virtualenv data-science -p /usr/local/opt/python@3.8/bin/python3
+
+
+
 ```bash
 source data-science/bin/activate
 ```
 
+python --version
 
 Install python dependencies
+
+pip install -r requirements
 
 ```bash
 python -m pip install -U matplotlib
@@ -38,3 +48,9 @@ pip install -U scikit-learn
 pip install -U pandas
 
 pip install -U matplotlib
+
+### Anaconda Installation
+
+Homwbrew?
+
+anaconda.com
---
+++ data-science/svm/basics.py
@@ -0,0 +1,77 @@
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+iris = datasets.load_iris()
+
+X = iris.data
+Y = iris.target
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = \
+	train_test_split(X, Y, test_size=0.3, random_state=1234, stratify=Y)
+
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+
+svc = SVC(kernel='rbf', gamma=1.7)
+svc.fit(x_train, y_train)
+Y_predict = svc.predict(x_test)
+cm_rbf01 = confusion_matrix(y_test, Y_predict)
+
+svc = SVC(kernel='rbf', gamma=10.0)
+svc.fit(x_train, y_train)
+Y_predict = svc.predict(x_test)
+cm_rbf10 = confusion_matrix(y_test, Y_predict)
+
+svc = SVC(kernel='poly')
+svc.fit(x_train, y_train)
+Y_predict = svc.predict(x_test)
+cm_poly = confusion_matrix(y_test, Y_predict)
+
+svc = SVC(kernel='sigmoid')
+svc.fit(x_train, y_train)
+Y_predict = svc.predict(x_test)
+cm_sigmoid = confusion_matrix(y_test, Y_predict)
+
+svc = SVC(kernel='linear')
+svc.fit(x_train, y_train)
+Y_predict = svc.predict(x_test)
+cm_linear = confusion_matrix(y_test, Y_predict)
+
+
+
+
+formatter = plt.FuncFormatter(lambda i, *args: iris.target_names[int(i)])
+
+
+# plt.figure(figsize=(5, 4))
+# plt.scatter(iris.data[:, 0], iris.data[:, 1], c=iris.target)
+# plt.colorbar(ticks=[0, 1, 2], format=formatter)
+# plt.xlabel(iris.feature_names[0])
+# plt.ylabel(iris.feature_names[1])
+
+# plt.figure(figsize=(5, 4))
+# plt.scatter(iris.data[:, 2], iris.data[:, 3], c=iris.target)
+# plt.colorbar(ticks=[0, 1, 2], format=formatter)
+# plt.xlabel(iris.feature_names[2])
+# plt.ylabel(iris.feature_names[3])
+
+fig = plt.figure("My Default Figure", dpi=72, facecolor='w', edgecolor='k')
+
+ax = fig.add_subplot(1, 2, 1)
+ax.set_xlabel(iris.feature_names[0])
+ax.set_ylabel(iris.feature_names[1])
+# fig.colorbar(ax)
+ax.scatter(iris.data[:, 0], iris.data[:, 1], c=iris.target)
+
+ax1 = fig.add_subplot(1, 2, 2)
+ax1.set_xlabel(iris.feature_names[2])
+ax1.set_ylabel(iris.feature_names[3])
+# ax.colorbar(ticks=[0, 1, 2], format=formatter)
+ax1.scatter(iris.data[:, 2], iris.data[:, 3], c=iris.target)
+
+
+# fig.colorbar()
+
+plt.tight_layout()
+plt.show()
